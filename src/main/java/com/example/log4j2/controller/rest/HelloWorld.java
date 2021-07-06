4631db09744e933fcaf4579c154227189e0c7e3a
Tuan Anh Tran <tuantrananhit@gmail.com>
2021-07-06 10:46:44 +0700

    Add Helloworld controller

---
+++ src/main/java/com/example/log4j2/controller/rest/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.example.log4j2.controller.rest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RequestMapping("/api")
+@RestController
+public class HelloWorld {
+  private static final Logger logger = LogManager.getLogger();
+  public String helloWorld() {
+    String requestKey = UUID.randomUUID().toString();
+    MDC.put("request_id", requestKey);
+    logger.debug("Debug Message Logged !!!");
+    logger.info("Info Message Logged !!!");
+    return "hello world";
+  }
+}
